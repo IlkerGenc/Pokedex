119e4684180c0683b0bcb73fc977ea7b96ecf10c
Ilker Genc <ilker.genc@hotmail.de>
2025-07-28 15:21:14 +0200

    function left & right arrow

--- remoteStorage.js
+++ remoteStorage.js
@@ -2,6 +2,9 @@ function onloadFunc() {
   loadPokemonNames();
 }
 
+let allPokemon = []; // globale Liste speichern
+let currentIndex = 0;
+
 let offset = 0;
 const limit = 20;
 const baseURL = 'https://pokeapi.co/api/v2/pokemon';
@@ -11,11 +14,13 @@ async function loadPokemonNames() {
   const response = await fetch(url);
   const data = await response.json();
 
+  allPokemon = allPokemon.concat(data.results); // Liste erweitern
+
   const container = document.getElementById('main_content');
 
-  data.results.forEach(item => {
+  data.results.forEach((item, index) => {
     const cardHTML = `
-      <div class="pokemon-card" onclick="showOverlay('${item.name}')">
+      <div class="pokemon-card" onclick="showOverlay(${offset + index})">
       <div class="pokemon-name">${item.name}</div>
       </div>
     `;
@@ -23,15 +28,33 @@ async function loadPokemonNames() {
   });
 }
 
-function showOverlay(pokemonName) {
-  const overlay = document.getElementById('pokemon-overlay');
+function showOverlay(index) {
+  currentIndex = index;
+  const pokemonName = allPokemon[index].name;
+  // const overlay = document.getElementById('pokemon-overlay');
   const nameContainer = document.getElementById('overlay-pokemon-name');
   nameContainer.textContent = pokemonName;
-  overlay.classList.remove('hidden');
+
+
+  document.getElementById('left-arrow').style.display = index === 0 ? 'none' : 'inline';
+  document.getElementById('right-arrow').style.display = index === allPokemon.length - 1 ? 'none' : 'inline';
+  document.getElementById('pokemon-overlay').classList.remove('hidden');
 }
 
 function closeOverlay() {
   document.getElementById('pokemon-overlay').classList.add('hidden');
+}
+
+function showNextPokemon() {
+  if (currentIndex < allPokemon.length - 1) {
+    showOverlay(currentIndex + 1);
+  }
+}
+
+function showPreviousPokemon() {
+  if (currentIndex > 0) {
+    showOverlay(currentIndex - 1);
+  }
 }
 
 function loadMorePokemon() {
